30b8303a86687e1c37a165be2f8fdd1faa04938e
Kun Yan <kyan@redhat.com>
2019-03-11 15:41:01 +0800

    optimization bundle size

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,14 +1,17 @@
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+// import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
 
 const config: webpack.Configuration = {
   externals: [
     {
       react: 'React',
       'react-dom': 'ReactDOM',
-      '@material-ui/core': 'window["material-ui"]'
+      '@material-ui/core': 'window["material-ui"]',
+      'highlight.js': 'hljs',
+      esprima: 'esprima',
+      'js-yaml': 'jsyaml'
     }
-    // /@material-ui\/core\/.*/
   ],
   module: {
     rules: [
@@ -22,9 +25,34 @@ const config: webpack.Configuration = {
     new HtmlWebpackPlugin({
       template: './src/index.html'
     })
+    // new BundleAnalyzerPlugin(),
   ],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          chunks: 'initial',
+          minChunks: 2,
+          maxInitialRequests: 5,
+          minSize: 0
+        },
+        vendors: {
+          name: 'vendors',
+          chunks: 'initial',
+          test: /[\\/]node_modules[\\/]/,
+          priority: 10,
+          enforce: true
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
+    }
   }
 };
 
